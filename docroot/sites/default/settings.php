79e90068b7a9610f3104aeb16b40a4bd3d603c8c
rjjime <rjjime@linuxossupport.com>
2024-11-26 21:39:04 -0500

    feat: update local development setup and configuration

---
+++ docroot/sites/default/settings.php
@@ -0,0 +1,15 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 's-iYf3_tFy6P8l_Of8oroynqlm2g1zK5MxAucQrD-bcfszQS6oq8rZ-Jr97ZNn5cgYHSSwXYJg';
+$settings['config_sync_directory'] = 'sites/default/files/config_rkiQOrJ3FeYCdwydiCjoQ8apVvYeS59kesxxQOVr7qnaB-uZpZH-lEUg4YrVnEdNvxl-3eyZHA/sync';
